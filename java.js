6e08d7bfc716d2be4fa2fe9a58e4c4336baa96ad
Julian <65081326+shulians9@users.noreply.github.com>
2023-02-28 17:52:32 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
+
+document.getElementById('boton').addEventListener('click', function (){
+    document.getElementById("demo").innerHTML = "ARGENTINA PROGRAMA 4.0";
+})
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Currículum-CV-Diseño
+Cómo crear el Curriculum Vitae/Diseño de CV usando HTML , CSS y algunos eventos
+en JS
